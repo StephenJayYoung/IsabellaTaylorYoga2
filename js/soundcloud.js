d3c3d1dff05bfc89a64010b427f2ed5d40f66428
Young Creative Space <StephenJayYoung@gmail.com>
2015-03-11 11:13:11 -0700

    more edits to soundlcoud api

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,13 +1,13 @@
 // initialize soundcloud API with key
 SC.initialize({
     // This is the sample client_id. you should replace this with your own
-    client_id: "887b335a80f3e625454ebca548c53d96"
+    client_id: "enter id here"
 });
 
 function embed (id) {
   SC.oEmbed("http://soundcloud.com/" + id // user or playlist to embed
     , { color: "ff0066"
-      , auto_play: true
+      , auto_play: true //should this be false?
       , maxwidth: 500
       , maxheight: 1000
       , show_comments: true } // options
@@ -17,6 +17,6 @@ function embed (id) {
 
 $(document).ready(
   function () {
-    embed("dj-faze");
+    embed("dj-faze"); //should this be changed?
   }
 );
