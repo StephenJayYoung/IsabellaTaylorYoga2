08ac4aeba0eca271d309cc7217ade3c90bc881ac
Young Creative Space <StephenJayYoung@gmail.com>
2015-03-13 19:47:24 -0700

    added soundcould widget. will need some styling

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,25 +1,4 @@
-// // initialize soundcloud API with key
-// SC.initialize({
-//     // This is the sample client_id. you should replace this with your own
-//     client_id: "5c1c0e2bcfcc6d2b3a3df662f8a88ae5"
-// });
 
-// function embed (id) {
-//   SC.oEmbed("http://soundcloud.com/" + id // user or playlist to embed
-//     , { color: "ff0066"
-//       , auto_play: true //should this be false?
-//       , maxwidth: 500
-//       , maxheight: 1000
-//       , show_comments: true } // options
-//     , document.getElementById("scWidget") // what element to attach player to
-//   );
-// }
-
-// $(document).ready(
-//   function () {
-//     embed("dj-faze"); //should this be changed?
-//   }
-// );
 
 
 
